8ef0ab6d70a34271e483da30ab87af4c6b2225de
drenomor <johnnyfromhun@gmail.com>
2021-11-23 15:08:23 +0100

    Matek kitalálós

---
+++ feladatok/F0005c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+szam1 = 33
+szam2 = 73
+
+tipp = int(input('Mennyi ' + str(szam1) + ' + ' + str(szam2) + ' ? '))
+
+if tipp == szam1 + szam2:
+    print('Nem rossz, nem rossz...')
+else:
+    print('Nono!')
