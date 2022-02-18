5361a9f6c5dec165a0be4aaee56e43ddfdc5591c
drenomor <johnnyfromhun@gmail.com>
2022-02-18 15:44:52 +0100

    Túrázunk?

---
+++ peldak/P0006_tura.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+evszak = input('Melyik évszak van? (ny/ő) : ')
+esik_az_eso = input('Esik az eső? (i/n) : ')
+fuj_a_szel = input('Fúj a szél? (i/n) :')
+
+if evszak == 'ny' and fuj_a_szel == 'n':
+    print('Megyünk ki!')
+if evszak == 'ő' and esik_az_eso == 'n' and fuj_a_szel == 'n':
+    print('Megyünk ki!')
