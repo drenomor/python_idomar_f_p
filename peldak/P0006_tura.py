57da5e735b2ac7bdd16e9ef8ae4219b5d517822a
drenomor <johnnyfromhun@gmail.com>
2022-02-21 15:37:53 +0100

    Tura - 02

--- peldak/P0006_tura.py
+++ peldak/P0006_tura.py
@@ -4,9 +4,7 @@ evszak = input('Melyik évszak van? (ny/ő) : ')
 esik_az_eso = input('Esik az eső? (i/n) : ')
 fuj_a_szel = input('Fúj a szél? (i/n) : ')
 
-if evszak == 'ny' and fuj_a_szel == 'n':
-    print('Megyünk ki!')
-elif evszak == 'ő' and esik_az_eso == 'n' and fuj_a_szel == 'n':
+if fuj_a_szel == 'n' and (evszak == 'ny' or (evszak == 'ő' and esik_az_eso == 'n')):
     print('Megyünk ki!')
 else:
     print('Maradunk otthon!')
