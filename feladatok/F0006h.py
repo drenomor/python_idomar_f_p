091c0ab4906d61302233ab6f6050e21d9a02a20f
drenomor <johnnyfromhun@gmail.com>
2022-02-22 12:59:03 +0100

    Gondoltam egy számra - 01

---
+++ feladatok/F0006h.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+gondolt_szam = int(input('Gondolt szám : '))
+tipp = int(input('Melyik számra gondolok : '))
+if gondolt_szam == tipp:
+    print('Eltaláltad, ügyes vagy!')
+elif tipp == (gondolt_szam - 1) or tipp == (gondolt_szam + 1):
+    print('Majdnem sikerlült!')
+else:
+    print('Ez most nem jött össze, talán majd legközelebb.')
+print('Viszlát')
