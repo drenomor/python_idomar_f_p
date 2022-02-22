6628c4136100ea5c5e05b14e1d7f2bd3e180c96e
drenomor <johnnyfromhun@gmail.com>
2022-02-22 08:45:29 +0100

    F0006b - int()

--- feladatok/F0006b.py
+++ feladatok/F0006b.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-a = input('Kérem az első számot: ')
-b = input('Kérem a második számot: ')
+a = int(input('Kérem az első számot: '))
+b = int(input('Kérem a második számot: '))
 
 if a < b:
     szoveg = 'A két szám közül a kisebb: ' + str(a)
