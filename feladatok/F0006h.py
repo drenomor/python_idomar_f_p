46dc1d97a1cdfe60172f4081ce07cda696403437
drenomor <johnnyfromhun@gmail.com>
2022-02-22 13:29:23 +0100

    abs()

--- feladatok/F0006h.py
+++ feladatok/F0006h.py
@@ -4,7 +4,7 @@ gondolt_szam = int(input('Gondolt szám : '))
 tipp = int(input('Melyik számra gondolok : '))
 if gondolt_szam == tipp:
     print('Eltaláltad, ügyes vagy!')
-elif tipp == (gondolt_szam - 1) or tipp == (gondolt_szam + 1):
+elif abs(tipp - gondolt_szam) == 1:
     print('Majdnem sikerlült!')
 else:
     print('Ez most nem jött össze, talán majd legközelebb.')
