bc2cef016c2e533d3ae23edfe8140d933bae5f80
drenomor <johnnyfromhun@gmail.com>
2022-03-01 10:19:34 +0100

    Hányasával írjuk ki?

---
+++ feladatok/F0007f.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+print('A program kiírja a számokat a beírt határok között.\n')
+also_hatar = int(input('Kérem a tartomány alsó határát: '))
+felso_hatar = int(input('Kérem a tartomány felső határát: '))
+koz = int(input('Hányasával írjuk ki a számokat? : '))
+
+if also_hatar > felso_hatar:
+    seged = also_hatar
+    also_hatar = felso_hatar
+    felso_hatar = seged
+
+seged = also_hatar
+
+while seged <= felso_hatar:
+    print(seged)
+    seged += koz
