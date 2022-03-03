ce76c3129d68cde1dfcef70bca5010bf95502055
drenomor <johnnyfromhun@gmail.com>
2022-03-03 08:16:53 +0100

    F0008b.py - eltérés a tipptől

---
+++ feladatok/F0008b.py
@@ -0,0 +1,14 @@
+#!/us/bin/env python3
+gondolt_szam = 4
+kitalalta = False
+lehetoseg = 3
+
+while not kitalalta and lehetoseg > 0:
+    tipp = int(input('Melyik számra gondoltam 1 és 5 között? : '))
+    if tipp == gondolt_szam:
+        kitalalta = True
+    else:
+        print('Nem ez az a szám amire gondoltam! Az eltérés : ', abs(gondolt_szam - tipp))
+    lehetoseg -= 1
+
+print('Viszlát!')
