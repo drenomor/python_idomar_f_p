3c578aa9cd8edc6a1205863165b1e5c62df3c7bb
drenomor <johnnyfromhun@gmail.com>
2022-03-03 08:21:15 +0100

    F0008c.py - Kisebb vagy nagyobb segítség

---
+++ feladatok/F0008c.py
@@ -0,0 +1,16 @@
+#!/us/bin/env python3
+gondolt_szam = 4
+kitalalta = False
+lehetoseg = 3
+
+while not kitalalta and lehetoseg > 0:
+    tipp = int(input('Melyik számra gondoltam 1 és 5 között? : '))
+    if tipp == gondolt_szam:
+        kitalalta = True
+    elif tipp > gondolt_szam:
+        print('Kisebb számra gondoltam!')
+    elif tipp < gondolt_szam:
+        print('Nagyobb számra gondoltam!')
+    lehetoseg -= 1
+
+print('Viszlát!')
