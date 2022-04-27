51eb226f588f64a1422e2be2e54d6bf87524eedb
drenomor <johnnyfromhun@gmail.com>
2022-04-27 13:40:21 +0200

    Átló kirajzolása

---
+++ feladatok/F0011a.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+oszlop = 0
+while oszlop < 25:
+    sor = 0
+    while sor < oszlop: # egy sor kiíratása (egy sorban 25 db O van)
+        print(' ',end='')
+        sor += 1
+    print('O')
+    oszlop += 1
