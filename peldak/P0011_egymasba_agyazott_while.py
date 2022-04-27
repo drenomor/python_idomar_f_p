3fa96cc320b117bb2248927d6055a10fc38ae423
drenomor <johnnyfromhun@gmail.com>
2022-04-27 13:13:30 +0200

    P0011_egymasba_agyazott_while.py - 25 sor és oszlop kiíratása

---
+++ peldak/P0011_egymasba_agyazott_while.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+oszlop = 0
+while oszlop < 25:
+    sor = 0
+    while sor < 25: # egy sor kiíratása (egy sorban 25 db O van)
+        print('O',end='')
+        sor += 1
+    print('')
+    oszlop += 1
